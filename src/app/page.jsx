5066d876437fd1c2b0aafef05e1802c379d57f8c
Codiemaureen <codiestover.cs@gmail.com>
2025-03-03 13:58:55 -0500

    Adding style sheets linked and started

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 export const metadata = {
   title : `Nasa's Photo of the Day`
 }
+import styles from './page.module.css'; 
+
 
 export default function Home() {
   return (
-    <h1>Nasa Image Of the Day</h1>
+    <h1 className={styles.header}>Nasa Image Of the Day</h1>
   )
 }
