25a9d8c05fe129f41abdc2ccf9251db017166c48
Codiemaureen <codiestover.cs@gmail.com>
2025-03-03 13:30:24 -0500

    code clean up 🫧

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,9 @@
-
-
 export const metadata = {
-  icons: {
-    icon: "/favicon.ico"
-  },
   title : `Nasa's Photo of the Day`
 }
 
 export default function Home() {
   return (
     <h1>Nasa Image Of the Day</h1>
-  );
+  )
 }
